e55ceba3fd029b06d5456c18a693a0abdb3e819d
andreyegor <doesnoexist@gmail.com>
2022-11-12 22:01:59 +0700

    на вске

---
+++ bad_apple_on_text_file.py
@@ -0,0 +1,14 @@
+import transform_frames
+from time import sleep
+
+path = 'screen.txt'
+for i in range(1, 3970, 2):
+    with open(path, 'w+') as f:
+        f.seek(0)
+        braille = transform_frames.get_converted_image(
+            f'frames/frame{i}.jpg', width=230)
+        frame = ''
+        for e in braille:
+            frame += ''.join(e)+'\n'
+        f.write(frame+'\n'+str(i))
+        sleep(1/15)
