e1c89db1461babc3d7b905c2ccb14b6bf895b25d
andreyegor <doesnoexist@gmail.com>
2022-11-12 20:26:09 +0700

    медленная версия бота для дискорда

---
+++ discord_bot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+from time import sleep
+
+import transform_frames
+import config
+
+# я без понятия как сделать это быстрее
+token = config.settings.token[0]
+
+intent = discord.Intents.default()
+intent.members = True
+intent.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intent)
+
+
+@bot.command(pass_context=True)  # разрешаем передавать агрументы
+async def test(ctx, arg):  # создаем асинхронную фунцию бота
+    await ctx.send(arg)  # отправляем обратно аргумент
+
+
+@bot.command(pass_context=True)
+async def bad_apple(ctx):
+    await ctx.send(f"Нет блин, злая груша, а злая она потому что пинг {bot.latency} это довольно много")
+    sleep(1)
+    for i in range(393, 600):
+        braille = transform_frames.get_converted_image(
+            f'frames/frame{i}.jpg', width=150)
+        frame = ''
+        for e in braille:
+            frame += ''.join(e)+'\n'
+        await ctx.send(frame)
+
+
+bot.run(token)
