8bbadc83ad17307ca7ddcf8941e2eab06b2fb8a9
andreyegor <doesnoexist@gmail.com>
2022-11-12 22:58:41 +0700

    поправил тайминги

--- bad_apple_on_text_file.py
+++ bad_apple_on_text_file.py
@@ -2,7 +2,7 @@ import transform_frames
 from time import sleep
 
 path = 'screen.txt'
-for i in range(1, 3970, 2):
+for i in range(1, 3970, 3):
     with open(path, 'w+') as f:
         f.seek(0)
         braille = transform_frames.get_converted_image(
@@ -11,4 +11,4 @@ for i in range(1, 3970, 2):
         for e in braille:
             frame += ''.join(e)+'\n'
         f.write(frame+'\n'+str(i))
-        sleep(1/15)
+        sleep(0.0626)
