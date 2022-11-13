4320f0a50a750e6d5094e459430cab29a997b5e7
andreyegor <doesnoexist@gmail.com>
2022-11-13 17:40:11 +0700

    add config_example

---
+++ config_example.py
@@ -0,0 +1,13 @@
+#from dataclasses import dataclass
+
+
+class discord_bot_settings():
+    token = 'token'
+    prefix = '$'
+
+
+class telegram_bot_settings():
+    token = ('token1',
+             'token2',
+             'token3',)
+    controller_token = 'controller_token'
