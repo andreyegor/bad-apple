2eac9fea4045ae43e391c332ecd71cab16f02901
andreyegor <doesnoexist@gmail.com>
2022-11-14 16:18:47 +0700

    mono pattern added

--- on_text_file.py
+++ on_text_file.py
@@ -1,6 +1,9 @@
 import transform_frames
+import data
+
 from time import sleep
 
+transform_frames.pattern = data.mono_braille_pattern
 path = 'screen.txt'
 for i in range(1, 6571, 6):
     with open(path, 'w+') as f:
