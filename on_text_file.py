640e0c2ce50290acef6e00be9a121cb8a80f7c3a
andreyegor <doesnoexist@gmail.com>
2023-02-27 17:46:58 +0700

    -_-

--- on_text_file.py
+++ on_text_file.py
@@ -3,12 +3,12 @@ import data
 
 from time import sleep
 
-transform_frames.pattern = data.mono_braille_pattern
+#transform_frames.pattern = data.mono_braille_pattern
 path = 'screen.txt'
-for i in range(1, 6571, 6):
+for i in range(1, 6571, 3):
     with open(path, 'w+') as f:
         f.seek(0)
         frame = transform_frames.get_converted_image(
-            f'frames/frame{i}.jpg', width=230)
+            f'frames/frame{i}.jpg', width=300)
         f.write(frame+'\n'+str(i))
         sleep(0.0626)
